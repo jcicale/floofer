ea009c462547650c180aebd0067adce7a4f462f5
Julia <julia.cicale@gmail.com>
2017-10-09 21:26:46 -0500

    added some plugins, testing some onsen functionality

--- platforms/ios/www/js/test.js
+++ platforms/ios/www/js/test.js
@@ -1,11 +1,16 @@
-document.addEventListener('init', function(event) {
-  var page = event.target;
+var hideDialog = function (id) {
+    document.getElementById(id).hide();
+};
 
-  if (page.id === 'page1') {
-    page.querySelector('#push-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
-    };
-  } else if (page.id === 'page2') {
-    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-  }
-});
+var fromTemplate = function () {
+    var dialog = document.getElementById('initial-dialog');
+
+    if (dialog) {
+        dialog.show();
+    }
+    else {
+        ons.createDialog('dialog.html')
+            .then(function (dialog) {
+                dialog.show();
+            });
+    }
